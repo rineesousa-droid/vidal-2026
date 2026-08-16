529e0424bdd5613588864fe9236db9513880afbd
rineesousa-droid <rineesousa@gmail.com>
2026-08-16 13:21:40 -0300

    Atualizar logo cabecalho v2

--- src/components/site/SiteHeader.tsx
+++ src/components/site/SiteHeader.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Menu, X } from "lucide-react";
 
-import logo from "@/assets/logo-eduardo-vidal-horizontal.png";
+import logo from "@/assets/logo-cabecalho-eduardo-v2.png";
 import { nav } from "@/content/eduardo";
 
 export function SiteHeader() {
